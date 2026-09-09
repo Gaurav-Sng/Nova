2805b2d148ca8ef55ffdf1c81e9a71de448e45d2
Gaurav <145678761+Gaurav-Sng@users.noreply.github.com>
2026-09-09 16:09:32 +0530

    Change API base URL to production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://nova-4vn4.onrender.com/',
 });
 
 api.interceptors.request.use((config) => {
